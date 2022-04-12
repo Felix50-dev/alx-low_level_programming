890269be932a5f1256356906e61b4ceb6b22dcf7
Felix50-dev <mwatufelix1@gmail.com>
2022-04-12 08:40:15 -0700

    print name of compiled file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - print the name of compiled file
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	printf("the name of this file is: %s\n", __FILE__);
+	return (0);
+}
